501026c135c4a8042591f395270f68dadffdf783
4NDrew-42 <100322188+4NDrew-42@users.noreply.github.com>
2024-06-05 16:55:51 -0500

    Update index.js
    https://4ndrew-42.github.io/ArtCine-Web/

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://localhost:4200', 'http://testsite.com', 'https://artcine.netlify.app'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://localhost:4200', 'http://testsite.com', 'https://artcine.netlify.app', 'https://4ndrew-42.github.io/ArtCine-Web/'];
 
 app.use(
 	cors({
